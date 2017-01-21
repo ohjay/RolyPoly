39ba89f4a72bdbacbab2d36c99d09faf5e247963
Richard Gao <Richard Gao>
2017-01-21 14:25:54 -0800

    added capture button

--- app/src/main/java/xyz/owenjow/facialrecognizerer/CaptureActivity.java
+++ app/src/main/java/xyz/owenjow/facialrecognizerer/CaptureActivity.java
@@ -9,5 +9,6 @@ public class CaptureActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_capture);
+
     }
 }
