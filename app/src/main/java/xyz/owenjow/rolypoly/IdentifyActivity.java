c3362931af2ba247a799482ec6a392ab93a6e30a
Matthew Sit <msit@berkeley.edu>
2017-01-21 19:17:18 -0800

    move ai file

--- app/src/main/java/xyz/owenjow/rolypoly/IdentifyActivity.java
+++ app/src/main/java/xyz/owenjow/rolypoly/IdentifyActivity.java
@@ -7,13 +7,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-import xyz.owenjow.facialrecognizerer.R;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 
 public class IdentifyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
+
+
         setContentView(R.layout.activity_identify);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -28,4 +41,35 @@ public class IdentifyActivity extends AppCompatActivity {
         });
     }
 
+    //Source: http://docs.opencv.org/2.4/doc/tutorials/introduction/desktop_java/java_dev_intro.html#running-sbt-samples
+    // Detects faces in an image, draws boxes around them, and writes the results
+    // to "faceDetection.png".
+    //
+    class DetectFaceDemo {
+        public void run() {
+            System.out.println("\nRunning DetectFaceDemo");
+
+            // Create a face detector from the cascade file in the resources
+            // directory.
+            CascadeClassifier faceDetector = new CascadeClassifier(getClass().getResource("/lbpcascade_frontalface.xml").getPath().substring(1).replace("%20", " "));
+            Mat image = Imgcodecs.imread(getClass().getResource("/lena.png").getPath().substring(1).replace("%20", " "));
+
+            // Detect faces in the image.
+            // MatOfRect is a special container class for Rect.
+            MatOfRect faceDetections = new MatOfRect();
+            faceDetector.detectMultiScale(image, faceDetections);
+
+            System.out.println(String.format("Detected %s faces", faceDetections.toArray().length));
+
+            // Draw a bounding box around each face.
+            for (Rect rect : faceDetections.toArray()) {
+                Imgproc.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0));
+            }
+
+            // Save the visualized detection.
+            String filename = "faceDetection.png";
+            System.out.println(String.format("Writing %s", filename));
+            Imgcodecs.imwrite(filename, image);
+        }
+    }
 }
