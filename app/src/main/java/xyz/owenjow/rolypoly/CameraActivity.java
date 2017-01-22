beeabac80458f3adb7a56f260d2e662f0fe78eca
Sagang Wee <sagangwee@yahoo.com>
2017-01-21 17:56:37 -0800

    Yay no more syntax errors

--- app/src/main/java/xyz/owenjow/rolypoly/CameraActivity.java
+++ app/src/main/java/xyz/owenjow/rolypoly/CameraActivity.java
@@ -21,7 +21,7 @@ public class CameraActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_main);
 
         // Create an instance of Camera
         mCamera = getCameraInstance();
