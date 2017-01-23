da5f99f20323012969a1cc2678d7640df83874ad
Sagang Wee <sagangwee@yahoo.com>
2017-01-22 17:39:19 -0800

    Fixed orientation issues with face overlay

--- app/src/main/java/xyz/owenjow/rolypoly/CameraPreview.java
+++ app/src/main/java/xyz/owenjow/rolypoly/CameraPreview.java
@@ -1,9 +1,13 @@
 package xyz.owenjow.rolypoly;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.res.Configuration;
 import android.hardware.Camera;
+import android.hardware.SensorManager;
 import android.util.Log;
+import android.view.OrientationEventListener;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.ViewGroup;
@@ -11,12 +15,22 @@ import android.view.Window;
 import android.widget.LinearLayout;
 
 import java.io.IOException;
+import java.util.List;
 
 /** A basic Camera preview class */
 public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
     public static final String TAG = "YOUR-TAG-NAME";
     private SurfaceHolder mHolder;
     private Camera mCamera;
+
+    // We need the phone orientation to correctly draw the overlay:
+    private int mOrientation;
+    private int mOrientationCompensation;
+    private OrientationEventListener mOrientationEventListener;
+
+    // Let's keep track of the display rotation and orientation also:
+    private int mDisplayRotation;
+    private int mDisplayOrientation;
 
     // Holds the Face Detection result:
     private Camera.Face[] mFaces;
@@ -46,6 +60,10 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         mFaceView.setLayoutParams(new ViewGroup.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.MATCH_PARENT));
+
+        // Create and Start the OrientationListener:
+        mOrientationEventListener = new SimpleOrientationEventListener(context);
+        mOrientationEventListener.enable();
 
         // Install a SurfaceHolder.Callback so we get notified when the
         // underlying surface is created and destroyed.
@@ -90,6 +108,22 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         // set preview size and make any resize, rotate or
         // reformatting changes here
 
+        // Get the supported preview sizes:
+        Camera.Parameters parameters = mCamera.getParameters();
+        List<Camera.Size> previewSizes = parameters.getSupportedPreviewSizes();
+        Camera.Size previewSize = previewSizes.get(0);
+        // And set them:
+        parameters.setPreviewSize(previewSize.width, previewSize.height);
+        mCamera.setParameters(parameters);
+        // Now set the display orientation for the camera. Can we do this differently?
+        mDisplayRotation = Util.getDisplayRotation(getActivity());
+        mDisplayOrientation = Util.getDisplayOrientation(mDisplayRotation, 0);
+        mCamera.setDisplayOrientation(mDisplayOrientation);
+
+        if (mFaceView != null) {
+            mFaceView.setDisplayOrientation(mDisplayOrientation);
+        }
+
         // start preview with new settings
         try {
             mCamera.setPreviewDisplay(mHolder);
@@ -99,20 +133,59 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
             Log.d(TAG, "Error starting camera preview: " + e.getMessage());
         }
 
-        Camera.Parameters parameters = mCamera.getParameters();
-        if (this.getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE) {
-            parameters.set("orientation", "portrait");
-            mCamera.setDisplayOrientation(90);
-            parameters.setRotation(90);
-            mCamera.startPreview();
+//        Camera.Parameters parameters = mCamera.getParameters();
+//        if (this.getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE) {
+//            parameters.set("orientation", "portrait");
+//            mCamera.setDisplayOrientation(90);
+//            parameters.setRotation(90);
+//            mCamera.startPreview();
+//        }
+//        else {
+//            // This is an undocumented although widely known feature
+//            parameters.set("orientation", "landscape");
+//            // For Android 2.2 and above
+//            mCamera.setDisplayOrientation(0);
+//            // Uncomment for Android 2.0 and above
+//            parameters.setRotation(0);
+//        }
+    }
+
+    /**
+     * We need to react on OrientationEvents to rotate the screen and
+     * update the views.
+     */
+    private class SimpleOrientationEventListener extends OrientationEventListener {
+
+        public SimpleOrientationEventListener(Context context) {
+            super(context, SensorManager.SENSOR_DELAY_NORMAL);
         }
-        else {
-            // This is an undocumented although widely known feature
-            parameters.set("orientation", "landscape");
-            // For Android 2.2 and above
-            mCamera.setDisplayOrientation(0);
-            // Uncomment for Android 2.0 and above
-            parameters.setRotation(0);
+
+        @Override
+        public void onOrientationChanged(int orientation) {
+            // We keep the last known orientation. So if the user first orient
+            // the camera then point the camera to floor or sky, we still have
+            // the correct orientation.
+            if (orientation == ORIENTATION_UNKNOWN) return;
+            mOrientation = Util.roundOrientation(orientation, mOrientation);
+            // When the screen is unlocked, display rotation may change. Always
+            // calculate the up-to-date orientationCompensation.
+            int orientationCompensation = mOrientation
+                    + Util.getDisplayRotation(getActivity());
+            if (mOrientationCompensation != orientationCompensation) {
+                mOrientationCompensation = orientationCompensation;
+                mFaceView.setOrientation(mOrientationCompensation);
+            }
         }
+    }
+
+    private Activity getActivity() {
+        Context context = getContext();
+        while (context instanceof ContextWrapper) {
+            if (context instanceof Activity) {
+                return (Activity)context;
+            }
+            context = ((ContextWrapper)context).getBaseContext();
+        }
+        return null;
     }
 }
